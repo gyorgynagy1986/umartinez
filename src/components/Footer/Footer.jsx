67396bd1f1bccdbaa215a02f916b2e4cd549a593
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-07-25 16:20:11 +0200

    new changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import style from "./Fotter.module.css";
 import Link from "next/link";
-import {Bebas_Neue } from 'next/font/google'
+import { Bebas_Neue } from "next/font/google";
 
-const bebas = Bebas_Neue({ subsets: ['latin'], weight: "400" })
-
-
-const bromoStyle = {
-  marginTop: "1px",
-  textDecorationLine: "unset",
-  color: "#184892",
-  fontSize: "12px",
-};
+const bebas = Bebas_Neue({ subsets: ["latin"], weight: "400" });
 
 const creatorlink = "https://studiobromo.hu/";
 
@@ -20,19 +12,26 @@ const Footer = () => {
     <footer className={style.container}>
       <div className={style.containerTextleft}>
         <p className={style.containerTextleftCookieMobile}>Cookie policy</p>
-       <Link href='/' ><h2 className={`${style.h2} ${bebas.className}`}>GRUPO10</h2></Link> 
+        <Link href="/">
+          <h2 className={`${style.h2} ${bebas.className}`}>GRUPO10</h2>
+        </Link>
         <div className={style.containerTextP}>
           <p className={style.containerTextleftP}></p>
         </div>
-        <Link className={style.learnMore} href="https://www.grupo10.hu/">Learn more about Grupo10</Link>
+        <Link className={style.learnMore} href="https://www.grupo10.hu/">
+          Learn more about Grupo10
+        </Link>
         <div className={style.containerCookie}>
           <div className={style.containerTextleftReserved}>
-            {" "}
             <p>
-              {" "}
               All rights reserved<span>©</span>
             </p>
-            <p>Grupo10.hu</p><p className={style.mobileTurn}>|</p><p>2023</p><p className={style.stdbromo}> <Link href="https://studiobromo.hu/">Designed and developed by Studiobromo</Link></p>
+            <p>Grupo10.hu</p>
+            <p className={style.mobileTurn}>|</p>
+            <p>2023</p>
+            <Link className={style.stdbromo} href={creatorlink}>
+              Designed and developed by Studio Bromo
+            </Link>
           </div>
           <p className={style.containerTextleftCookie}>Cookie policy</p>
         </div>
