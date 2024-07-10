d5feccbcd69d90e411acc4baba8b05858a0f3ead
Nagy Gyorgy <gyorgynagy1986@gmail.com>
2024-07-10 12:10:59 +0200

    photo update

---
+++ src/components/UI/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Spinner.module.css';
+
+const Spinner = () => {
+  return (
+    <div className={style.spinnerContainer}>
+      <div className={style.spinner}></div>
+    </div>
+  );
+};
+
+export default Spinner;
