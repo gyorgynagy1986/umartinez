554a449519d7b4ae64ba1063fc936930b7bac4f0
Nagy Gyorgy <gyorgynagy1986@gmail.com>
2024-07-11 17:11:16 +0200

    new menu

--- src/app/en/page.jsx
+++ src/app/en/page.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+
+import { useEffect, useState } from 'react';
+
 import Hero from "@/components/Hero/Hero";
 import SectionStory from "@/components/Story/Story";
 import Event from "@/components/Event/Event";
@@ -7,6 +10,8 @@ import JoinUs from "@/components/JoinUs/JoinUs";
 import Contact from "@/components/Contact/Contact";
 import Services from "@/components/Services/Services";
 import Divider from "@/components/UI/Divider/Divider";
+import VideoPopup from "@/components/UI/PopUp/VideoPopup";
+
 
 const links = {
   facebook:
@@ -16,9 +21,35 @@ const links = {
     "https://reservours.com/Ultramarinos Martínez/tablereservation?s=website",
 };
 
+const POPUP_KEY = 'videoPopupClosedAt';
+const POPUP_TIMEOUT = 30 * 60 * 1000; // 30 minutes in milliseconds
+
+export default function Home() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const lastClosedAt = localStorage.getItem(POPUP_KEY);
+    const now = new Date().getTime();
+
+    if (!lastClosedAt || (now - lastClosedAt) > POPUP_TIMEOUT) {
+      const timer = setTimeout(() => {
+        setShowPopup(true);
+      }, 1000); // Show the popup after 1 second
+
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+    localStorage.setItem(POPUP_KEY, new Date().getTime());
+  };
+
 export default function Home() {
   return (
     <>
+          {showPopup && <VideoPopup videoUrl="https://www.youtube.com/embed/zljsi4r9v4s" onClose={handleClosePopup} />}
+
       <Hero lang={true} reservation={links.reservation} />
       <main>
         <Divider marginPrefix={true} />
